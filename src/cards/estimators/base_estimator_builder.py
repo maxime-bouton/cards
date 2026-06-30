88325dd008dd32f763ca088face8275459e2f673
Maxime Bouton <50295279+maxime-bouton@users.noreply.github.com>
2026-06-30 16:44:54 +0200

    [skip ci] refactor: estimator -> estimators

---
+++ src/cards/estimators/base_estimator_builder.py
@@ -0,0 +1,62 @@
+r"""Abstract class used to build Bayesian estimators."""
+
+from abc import ABC, abstractmethod
+
+import cards.backend as xp
+from cards.transition_kernels.base_transition_kernel import BaseTransitionKernel
+
+
+class BaseEstimatorBuilder(ABC):
+    """Abstract class underlying the computation of Bayesian estimates.
+
+    Parameters
+    ----------
+    var : BaseTransitionKernel
+        Transition kernel associated with the variable of interest to be estimated.
+    var_name : str
+        Name of the variable of interest to be estimated.
+    """
+
+    def __init__(
+        self,
+        var: BaseTransitionKernel,
+        var_name: str = "X",
+        to_cpu: bool = True,
+    ) -> None:
+        self._var = var
+        self._var_name = var_name
+        self._estimates: dict[str, xp.ndarray] = {}
+        self._to_cpu = to_cpu
+
+    @abstractmethod
+    def aggregate_states(self) -> None:
+        """Update running statistics with a new sample to compute the estimates with an
+        online approach (when possible)."""
+        pass
+
+    @abstractmethod
+    def build_estimates(self) -> None:
+        """Finalize the computation of the estimates."""
+        pass
+
+    @abstractmethod
+    def reset(self) -> None:
+        """Reset the value of the estimates to 0."""
+        pass
+
+    def get_estimates(self) -> dict[str, xp.ndarray]:
+        """Return the final estimates of the variable of interest."""
+        if self._to_cpu:
+            return {k: v.get() for k, v in self._estimates.items()}
+        return self._estimates
+
+    def setup(self, ckpt_size: int) -> None:
+        """Setup the estimator to be used for the application of interest.
+
+        Parameters
+        ----------
+        ckpt_size : int
+            Number of samples per checkpoint to be used for the computation of the
+            estimates.
+        """
+        pass
---
+++ src/cards/estimators/mmse_var_builder.py
@@ -0,0 +1,42 @@
+r"""Implementation of the MMSE estimate using an online computing approach."""
+
+import cards.backend as xp
+from cards.estimators.base_estimator_builder import BaseEstimatorBuilder
+from cards.transition_kernels.base_transition_kernel import BaseTransitionKernel
+
+
+class MMSEVarBuilder(BaseEstimatorBuilder):
+    r"""Implementation of an MMSE estimator.
+
+    Computation is carried out online.
+    """
+
+    def __init__(
+        self,
+        var: BaseTransitionKernel,
+        var_name: str = "X",
+    ):
+        super().__init__(var, var_name)
+        self._count = 0
+        self._mean = xp.zeros_like(self._var.current_state)
+        self._m2 = xp.zeros_like(self._var.current_state)
+
+    def aggregate_states(self) -> None:
+        """Update running statistics with new samples using Welford's algorithm."""
+        self._count += 1
+        delta = self._var.current_state - self._mean
+        self._mean += delta / self._count
+        delta2 = self._var.current_state - self._mean
+        self._m2 += delta * delta2
+
+    def build_estimates(self) -> None:
+        """Finalize both mean and variance computations."""
+        self._estimates = {
+            f"{self._var_name}_mmse": self._mean,
+            f"{self._var_name}_var": self._m2 / (self._count - 1),
+        }
+
+    def reset(self) -> None:
+        self._count = 0
+        self._mean.fill(0)
+        self._m2.fill(0)
