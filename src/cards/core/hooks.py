45ae61569c8a19f6a9cea83cb735f61c498278e5
Maxime Bouton <50295279+maxime-bouton@users.noreply.github.com>
2026-08-13 03:43:37 +0200

    feat: rework of geometry hook

--- src/cards/core/hooks.py
+++
@@ -1,9 +0,0 @@
-from typing import Protocol, TypeVar
-
-from cards.core.execution_context import ExecutionContext
-
-G = TypeVar("G")
-
-
-class GeometryHook(Protocol[G]):
-    def build_geometry(self, cfg: dict, ctx: ExecutionContext) -> G: ...
