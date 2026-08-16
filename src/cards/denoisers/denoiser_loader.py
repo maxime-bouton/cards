928971a2f9566143d2cbe5f3eca26663b8d1bbbb
Maxime Bouton <50295279+maxime-bouton@users.noreply.github.com>
2026-08-16 17:43:31 +0200

    fix: proper nn loading on devices without torch.cuda

--- src/cards/denoisers/denoiser_loader.py
+++ src/cards/denoisers/denoiser_loader.py
@@ -4,9 +4,18 @@ from pathlib import Path
 
 import torch
 
+import cards.backend as xp
 from cards.denoisers.ddfb.network_ddfb import DDFB
 from cards.denoisers.dncnn.network_dncnn import DnCNN
 from cards.denoisers.drunet.network_drunet import DRUNet
+
+
+def get_torch_device() -> torch.device:
+    """Read the current cards backend and return the corresponding PyTorch device."""
+    if xp.get_backend() == "cupy":
+        return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    else:
+        return torch.device("cpu")
 
 
 def load_pretrained_ddfb(
@@ -37,9 +46,11 @@ def load_pretrained_ddfb(
     DDFB
         An instance of the DDFB model with the specified weights.
     """
+    device = get_torch_device()
     weights = weights_path / f"ddfb_nch{C}_nla{n_layers}_nfe{n_features}.pth"
     net = DDFB(C=C, n_layers=n_layers, n_features=n_features)
-    net.load_state_dict(torch.load(weights, weights_only=True))
+    net.load_state_dict(torch.load(weights, weights_only=True, map_location=device))
+    net.to(device)
     net.eval()
     return net
 
@@ -48,9 +59,11 @@ def load_pretrained_drunet(
     nch=3,
     weights_path=Path(__file__).parents[3] / "data/weights/drunet",
 ) -> DRUNet:
+    device = get_torch_device()
     weights = weights_path / f"drunet_nch{nch}.pth"
     net = DRUNet(in_nc=nch + 1, out_nc=nch, act_mode="R", bias=False)
-    net.load_state_dict(torch.load(weights, weights_only=True))
+    net.load_state_dict(torch.load(weights, weights_only=True, map_location=device))
+    net.to(device)
     net.eval()
     return net
 
@@ -59,9 +72,11 @@ def load_pretrained_dncnn(
     nch=3,
     weights_path=Path(__file__).parents[3] / "data/weights/dncnn",
 ) -> DnCNN:
+    device = get_torch_device()
     weights = weights_path / f"dncnn_nch{nch}.pth"
     net = DnCNN(in_nc=nch, out_nc=nch, act_mode="R")
-    net.load_state_dict(torch.load(weights, weights_only=True))
+    net.load_state_dict(torch.load(weights, weights_only=True, map_location=device))
+    net.to(device)
     net.eval()
     return net
 
