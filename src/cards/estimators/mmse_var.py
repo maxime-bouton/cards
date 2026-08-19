b4bdac9fd39e581cadb20d8f891273e34eed2d89
Maxime Bouton <50295279+maxime-bouton@users.noreply.github.com>
2026-08-19 11:32:32 +0200

    feat: adaptation and improvement of memory efficiency of mmse var estimator

--- src/cards/estimators/mmse_var.py
+++ src/cards/estimators/mmse_var.py
@@ -1,8 +1,8 @@
 r"""Implementation of the MMSE estimate using an online computing approach."""
 
 import cards.backend as xp
+from cards.core.variable import Variable
 from cards.estimators.base_estimator import BaseEstimator
-from cards.transition_kernels.base_transition_kernel import BaseTransitionKernel
 
 
 class MMSEVar(BaseEstimator):
@@ -11,29 +11,37 @@ class MMSEVar(BaseEstimator):
     Computation is carried out online.
     """
 
-    def __init__(
-        self,
-        var: BaseTransitionKernel,
-        var_name: str = "X",
-    ):
-        super().__init__(var, var_name)
+    def __init__(self, var: Variable):
+        super().__init__(var)
         self._count = 0
-        self._mean = xp.zeros_like(self._var.current_state)
-        self._m2 = xp.zeros_like(self._var.current_state)
+        self._mean = xp.zeros_like(self._var.state)
+        self._m2 = xp.zeros_like(self._var.state)
+
+        self._delta = xp.zeros_like(self._var.state)
+        self._delta2 = xp.zeros_like(self._var.state)
 
     def aggregate_states(self) -> None:
         """Update running statistics with new samples using Welford's algorithm."""
         self._count += 1
-        delta = self._var.current_state - self._mean
-        self._mean += delta / self._count
-        delta2 = self._var.current_state - self._mean
-        self._m2 += delta * delta2
+
+        xp.subtract(self._var.state, self._mean, out=self._delta)
+
+        self._delta /= self._count
+        self._mean += self._delta
+
+        xp.subtract(self._var.state, self._mean, out=self._delta2)
+
+        self._delta *= self._count
+        self._delta *= self._delta2
+        self._m2 += self._delta
 
     def build_estimates(self) -> None:
         """Finalize both mean and variance computations."""
+        v = self._m2 / (self._count - 1) if self._count > 1 else xp.zeros_like(self._m2)
+
         self._estimates = {
-            f"{self._var_name}_mmse": self._mean,
-            f"{self._var_name}_var": self._m2 / (self._count - 1),
+            f"{self._var.name}_mmse": self._mean,
+            f"{self._var.name}_var": v,
         }
 
     def reset(self) -> None:
