6feb12b506554c863fca75955938306a27f2aa9f
Maxime Bouton <50295279+maxime-bouton@users.noreply.github.com>
2026-08-19 11:31:52 +0200

    feat: base estimator takes variable as parameter

--- src/cards/estimators/base_estimator.py
+++ src/cards/estimators/base_estimator.py
@@ -3,7 +3,7 @@ r"""Abstract class used to build Bayesian estimators."""
 from abc import ABC, abstractmethod
 
 import cards.backend as xp
-from cards.transition_kernels.base_transition_kernel import BaseTransitionKernel
+from cards.core.variable import Variable
 
 
 class BaseEstimator(ABC):
@@ -11,44 +11,13 @@ class BaseEstimator(ABC):
 
     Parameters
     ----------
-    var : BaseTransitionKernel
-        Transition kernel associated with the variable of interest to be estimated.
-    var_name : str
-        Name of the variable of interest to be estimated.
+    var : Variable
+        Variable of interest to be estimated.
     """
 
-    def __init__(
-        self,
-        var: BaseTransitionKernel,
-        var_name: str = "X",
-        to_cpu: bool = True,
-    ) -> None:
+    def __init__(self, var: Variable) -> None:
         self._var = var
-        self._var_name = var_name
         self._estimates: dict[str, xp.ndarray] = {}
-        self._to_cpu = to_cpu
-
-    @abstractmethod
-    def aggregate_states(self) -> None:
-        """Update running statistics with a new sample to compute the estimates with an
-        online approach (when possible)."""
-        pass
-
-    @abstractmethod
-    def build_estimates(self) -> None:
-        """Finalize the computation of the estimates."""
-        pass
-
-    @abstractmethod
-    def reset(self) -> None:
-        """Reset the value of the estimates to 0."""
-        pass
-
-    def get_estimates(self) -> dict[str, xp.ndarray]:
-        """Return the final estimates of the variable of interest."""
-        if self._to_cpu:
-            return {k: v.get() for k, v in self._estimates.items()}
-        return self._estimates
 
     def setup(self, ckpt_size: int) -> None:
         """Setup the estimator to be used for the application of interest.
@@ -59,4 +28,37 @@ class BaseEstimator(ABC):
             Number of samples per checkpoint to be used for the computation of the
             estimates.
         """
-        pass
+
+    @abstractmethod
+    def aggregate_states(self) -> None:
+        """Update running statistics with a new sample to compute the estimates with an
+        online approach (when possible)."""
+
+    @abstractmethod
+    def build_estimates(self) -> None:
+        """Finalize the computation of the estimates."""
+
+    @abstractmethod
+    def reset(self) -> None:
+        """Reset the value of the estimates to 0."""
+
+    @property
+    def estimates(self) -> dict[str, xp.ndarray]:
+        """Return the final estimates of the variable of interest."""
+        return self._estimates
+
+    @property
+    def local_sizes(self) -> dict:
+        r"""Return sizes of the local buffers representing the portion of the variables
+        handled by each MPI worker."""
+        return {n: self._var.layout.tile for n in self._estimates}
+
+    @property
+    def global_sizes(self) -> dict:
+        r"""Return the sizes of the global buffers representing the variables to be sampled."""
+        return {n: self._var.layout.full for n in self._estimates}
+
+    @property
+    def slices(self) -> dict:
+        r"""Return the slices to select local variables from the global memory buffer."""
+        return {n: self._var.layout.s for n in self._estimates}
