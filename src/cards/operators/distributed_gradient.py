fix: fixing issue in gradient operator

src/cards/operators/distributed_gradient.py
@@ -107,13 +107,8 @@ class DistributedGradient2d(LinearOperator):
             np.s_[:],
             *self.direct_communicator.cartslicer.slice_global_buffer_to_tile,
         )
-        self.adjoint_tile_size = (
-            np.asarray(
-                [
-                    2,
-                    *self.direct_communicator.cartslicer.tile_size,
-                ]
-            ),
+        self.adjoint_tile_size = np.asarray(
+            [2, *self.direct_communicator.cartslicer.tile_size]
         )
 
         # TODO: mutualize is_first/last -> communications ?

src/cards/slicers/cartesian_tessellation.py
@@ -144,7 +144,7 @@ def local_split_range(
         start = -1 + index * step
     stop = np.rint(start + step)
     start = np.rint(start)
-    rg = np.array([start + 1, stop], dtype="i")
+    rg = np.asarray([start + 1, stop])
     # if the facet overlaps with a neighbour
     if overlap > 0 and nchunks > 1:
         if backward: