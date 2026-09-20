59beec1a838fe8a84f92a51a49759b57ed388fd7
pthouvenin <34160817+pthouvenin@users.noreply.github.com>
2026-09-20 14:33:21 +0200

    feat: add testing error messages for dft convolution, do generate full arrays in mpi setting

--- tests/operators/test_dft_convolution.py
+++ tests/operators/test_dft_convolution.py
@@ -1,3 +1,5 @@
+r"""Testing implementation consistency and error messages for DFT-based convolution operators."""
+
 import numpy as np
 import pytest
 from mpi4py import MPI
@@ -7,7 +9,7 @@ from cards.operators.dft_convolution import DftConvolution
 from cards.operators.distributed_dft_convolution import DistributedDftConvolution
 from cards.utils.utils import expand_shape_left
 
-# FIXME: cleanse distributed test, avoid copying full array on all workers
+# FIXME: improve rng management for mpi tests (use create_rng, separate cpu and gpu configs)
 
 
 @pytest.fixture
@@ -16,16 +18,39 @@ def kernel_size(input_shape) -> np.ndarray:
 
 
 @pytest.mark.serial
+def test_dftconv_error_messages(input_size, kernel_size):
+
+    data_size = input_size + kernel_size - 1
+
+    # error complex-valued kernels
+    with pytest.raises(TypeError) as excinfo:
+        DftConvolution((*input_size,), (*data_size,), (1 + 1j) * xp.ones(kernel_size))
+    assert "only real-valued kernel supported" in str(excinfo.value)
+
+    # error kernel size (number of axes incompatiable with input shape)
+    with pytest.raises(ValueError) as excinfo:
+        DftConvolution((*input_size,), (*data_size,), xp.ones(kernel_size[-1:]))
+    assert "kernel should have ndims = len(image_size) dimensions" in str(excinfo.value)
+
+    # error input and output shape (inconsistent number of axes between input and output)
+    with pytest.raises(ValueError) as excinfo:
+        DftConvolution((*input_size,), (*data_size[-2:],), xp.ones(kernel_size))
+    assert "image_shape and data_shape must have the same number of elements" in str(
+        excinfo.value
+    )
+
+
+@pytest.mark.serial
 def test_adjoint(seed, input_shape, input_size, kernel_size):
     """
     Serial test checking the implementation of the adjoint operator is consistent with the direct operator.
     """
-    data_size = input_size + kernel_size - 1
     rng = xp.random.default_rng(seed)
 
-    x = rng.random(input_size)
-    y = rng.random(data_size)
     kernel = rng.random(kernel_size)
+    x = rng.random(input_size)
+    data_size = input_size + kernel_size - 1
+    y = rng.random(data_size)
 
     conv = DftConvolution(input_shape, (*data_size,), kernel)
 
@@ -35,26 +60,40 @@ def test_adjoint(seed, input_shape, input_size, kernel_size):
     Hxy = xp.sum(Hx * y)
     xHadj_y = xp.sum(x * Hadj_y)
 
-    xp.testing.assert_allclose(Hxy, xHadj_y)  # atol=1e-10
+    xp.testing.assert_allclose(Hxy, xHadj_y)
 
 
 @pytest.mark.mpi
-def test_adjoint_mpi(
-    seed, input_shape, input_size, kernel_size, comm, rank, grid_shape
-):
+def test_dftconv_error_messages_mpi(input_shape, kernel_size, comm, grid_shape):
+
+    # error complex-valued kernels
+    with pytest.raises(TypeError) as excinfo:
+        DistributedDftConvolution(
+            input_shape,
+            grid_shape,
+            comm,
+            (1 + 1j) * xp.ones(kernel_size),
+        )
+    assert "only real-valued kernel supported" in str(excinfo.value)
+
+    # error kernel size (number of axes incompatiable with input shape)
+    with pytest.raises(ValueError) as excinfo:
+        DistributedDftConvolution(
+            input_shape,
+            grid_shape,
+            comm,
+            (1 + 1j) * xp.ones(kernel_size[-2:]),
+        )
+    assert "kernel should have ndims = len(image_size) dimensions" in str(excinfo.value)
+
+
+@pytest.mark.mpi
+def test_adjoint_mpi(seed, input_shape, kernel_size, comm, rank, grid_shape):
     """
-    Test the adjoint property of the DFT convolution operator in distributed settings.
+    Distributed test checking the implementation of the adjoint operator is consistent with the direct operator.
     """
-    data_size = input_size + kernel_size - 1
-
-    rng = xp.random.default_rng(seed)
-
-    # draw local image time
-    x = rng.random(input_size)
-    y = rng.random(data_size)
-
-    kernel = rng.random(kernel_size)
-
+    kernel_rng = xp.random.default_rng(seed)
+    kernel = kernel_rng.random(kernel_size)
     convolution_handler = DistributedDftConvolution(
         input_shape,
         grid_shape,
@@ -62,12 +101,10 @@ def test_adjoint_mpi(
         kernel,
     )
 
-    local_x = x[
-        convolution_handler.direct_communicator.cartslicer.slice_global_buffer_to_tile
-    ]
-    local_y = y[
-        convolution_handler.adjoint_communicator.cartslicer.slice_global_buffer_to_tile
-    ]
+    # draw local image tile and observation tile
+    rng = xp.random.default_rng(seed)
+    local_x = rng.random(convolution_handler.direct_communicator.cartslicer.tile_size)
+    local_y = rng.random(convolution_handler.adjoint_communicator.cartslicer.tile_size)
 
     local_Hx = convolution_handler.forward(local_x)
     local_Hadj_y = convolution_handler.adjoint(local_y)
